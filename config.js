e06421af8b5f660252d079b9a102df73ae38fb0d
droplinxuser <68084306+droplinxuser@users.noreply.github.com>
2023-02-15 18:37:48 +0800

    Update config.js

--- config.js
+++ config.js
@@ -40,7 +40,7 @@ const config = {
   "uploadPass": false,
   // make the bucket-password field mandatory
   "requireBucketPassword": false,
-  "defaultRetention": "15780000",
+  "defaultRetention": "2630000",
   // expire every file after maxAge (eg never downloaded one-time files)
   "maxAge": 3600 * 24 * 30, // 30 days
   // maximum file-size for previews in byte
