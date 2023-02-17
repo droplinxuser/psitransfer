dd02de3542c84fe61d1a73fa40ad4f0881160a79
droplinxuser <68084306+droplinxuser@users.noreply.github.com>
2023-02-17 12:01:48 +0800

    Update config.js

--- config.js
+++ config.js
@@ -31,7 +31,7 @@ const config = {
     "2630000": "1 Month",
     "15780000": "6 Months",
     "31536000": "1 Year",
-    "189216000": "6 Years"
+    "315360000": "10 Years"
   },
   // admin password, set to false to disable /admin page
   "adminPass": 'temppasswordonly',
